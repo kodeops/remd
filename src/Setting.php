0751de187af305ae0b9383168c08270ff84d7000
Xavier Hernández <xavier@kodeops.io>
2021-07-23 11:57:37 +0200

    Add comments

--- src/Setting.php
+++ src/Setting.php
@@ -9,8 +9,10 @@ class Setting
 
     public static function get($service, $setting_key)
     {
+        // Build environment key according to given service and key
         $env_key = self::ENV_PREFIX . "_" . strtoupper($service) . "_{$setting_key}";
         
+        // Check that the environment key is set
         if (! env($env_key)) {
             throw new RemdPawException("remd is missing {$env_key} enviroment");
         }
